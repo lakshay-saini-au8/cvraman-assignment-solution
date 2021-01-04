8fe012c1d740e6aa024872b3661493a5230e19dd
Lakshay saini <lakshaysaini2013@gmail.com>
2021-01-04 23:44:08 +0530

    week19d3

---
+++ week19d3/app.js
@@ -0,0 +1,26 @@
+/*
+Create a JavaScript program to decode hashes as backspaces and return the
+deleted string.
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is
+"bd" Your task is to process a string with "#" symbols.
+(Note: ES6 is must)
+Examples: "abc#d##c" ==> "ac" "abc##d######" ==> "" "#######" ==> "" "" ==> ""
+*/
+
+// const res = [];
+// const input = process.argv[2];
+// for (const i in input) {
+//   if (i !== "#") {
+//     res.push(i);
+//   } else {
+//     if (res.length > 0) {
+//       res.pop();
+//     }
+//   }
+// }
+
+// console.log(res.join(""));
+const obj = {
+  firstName: "lakshay",
+  lastName: "saini",
+};
