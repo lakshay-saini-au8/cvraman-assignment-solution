707febc592c2c9046682982764eddcb5cacd1fda
Lakshay saini <lakshaysaini2013@gmail.com>
2021-02-01 23:28:09 +0530

    updated

--- weeek23day1/app.js
+++ weeek23day1/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import hbs from "express-handlebars";
 
 const app = express();
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+const data = [];
+const error = [];
 app.engine(
   ".hbs",
   hbs({
@@ -13,9 +14,22 @@ app.engine(
   })
 );
 app.set("view engine", ".hbs");
-
+app.post("/", function (req, res) {
+  const { name } = req.body;
+  if (name.length < 10) {
+    error.push({
+      field: "name",
+      message: "Name length should be greater than 10",
+    });
+    res.redirect("/");
+    return;
+  } else {
+    data.push(req.body);
+    res.redirect("/");
+  }
+});
 app.get("/", function (req, res) {
-  res.render("home", { title: "Home page" });
+  res.render("home", { data: data, error: error });
 });
 app.get("/login", function (req, res) {
   res.render("home", { layout: "login", title: "Home page" });
